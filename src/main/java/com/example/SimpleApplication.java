5e075dd768612c405dc989db73caf7e319e1afe8
Josh Long <josh@joshlong.com>
2021-12-13 13:02:26 -0800

    u

--- src/main/java/com/example/SimpleApplication.java
+++ src/main/java/com/example/SimpleApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.nativex.hint.ResourceHint;
 
+@ResourceHint ( patterns = "org/jobrunr/dashboard/frontend/build/.*")
 @SpringBootApplication
 public class SimpleApplication {
 
